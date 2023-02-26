a1b944d6d27e14dedba6b2fb8ecec9276f3e3e87
mahmud255 <tareqmahmud4@gmail.com>
2023-02-26 18:31:09 +0600

    Resource Files

---
+++ 4. Resources/01-Resource-Syntax/tf-manifest/ec2-instance.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "my-ec2-vm" {
+  ami               = "ami-082b1f4237bd816a1"
+  instance_type     = "t2.micro"
+  availability_zone = "ap-southeast-1a"
+  tags = {
+    "Name" = "web"
+  }
+
+}
