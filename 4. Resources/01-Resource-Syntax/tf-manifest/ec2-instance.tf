9868c02899855bfd8e284f8266fca36782a596d7
mahmud255 <tareqmahmud4@gmail.com>
2023-02-26 18:52:08 +0600

    file update

--- 4. Resources/01-Resource-Syntax/tf-manifest/ec2-instance.tf
+++ 4. Resources/01-Resource-Syntax/tf-manifest/ec2-instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "my-ec2-vm" {
+resource "aws_instance" "ec2-vm" {
   ami               = "ami-082b1f4237bd816a1"
   instance_type     = "t2.micro"
   availability_zone = "ap-southeast-1a"
