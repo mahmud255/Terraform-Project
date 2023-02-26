461458719b84abf6e1de1adb5acd7ed761fc8ee0
mahmud255 <tareqmahmud4@gmail.com>
2023-02-26 19:41:18 +0600

    file update

--- 4. Resources/01-Resource-Syntax/tf-manifest/ec2-instance.tf
+++ 4. Resources/01-Resource-Syntax/tf-manifest/ec2-instance.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "ec2-vm" {
-  ami               = "ami-082b1f4237bd816a1"
-  instance_type     = "t2.micro"
-  availability_zone = "ap-southeast-1a"
+  ami           = "ami-082b1f4237bd816a1"
+  instance_type = "t2.micro"
+  #availability_zone = "ap-southeast-1a"
+  availability_zone = "ap-southeast-1b"
+
   tags = {
     "Name" = "web"
     "ENV"  = "Dev"
