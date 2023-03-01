c55c41959fb0d00e5659fdddeb907737e79421e1
mahmud255 <tareqmahmud4@gmail.com>
2023-03-01 23:08:27 +0600

    file updated

--- 4. Resources/04-meta-argument-lifecycle/v3-ignore-changes/c2-ec2-instance.tf
+++ 4. Resources/04-meta-argument-lifecycle/v3-ignore-changes/c2-ec2-instance.tf
@@ -13,7 +13,6 @@ resource "aws_instance" "web" {
   lifecycle {
     ignore_changes = [
       # Ignore changes to tags, e.g. because a management agent
-      # updates these based on some ruleset managed elsewhere.
       tags,
 
     ]
